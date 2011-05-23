d1cf20b9ef1dce3c0039e79251aea995ff28a64d
Pavel Kotlyar <paxer@mail.ru>
2011-05-23 20:11:50 +1000

    added seed and email validator

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,5 +1,5 @@
 class Person < ActiveRecord::Base
   has_one :postcode
   validates :name, :postcode_id, :presence => true, :length => { :maximum => 50 }
-  validates :email, :presence => true, :format => { :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i }
+  validates :email, :presence => true, :email => true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
+require 'CSV'
+
+#Postcodes import
+csv_postcodes = File.read(File.join(Rails.root, 'db', 'seed_data', 'postcodes.csv'))
+csv_postcodes = CSV.parse(csv_postcodes, :headers => true)
+csv_postcodes.each do |row|
+  row = row.to_hash.with_indifferent_access
+  Postcode.create!(row.to_hash.symbolize_keys)
+end
+
+#People import
+csv_people = File.read(File.join(Rails.root, 'db', 'seed_data', 'people.csv'))
+csv_people = CSV.parse(csv_people, :headers => true)
+csv_people.each do |row|
+  row = row.to_hash.with_indifferent_access
+  Person.create!(row.to_hash.symbolize_keys)
+end
